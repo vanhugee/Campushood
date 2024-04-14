e6d213e74a0d2670ac09d3f204c5074fee809764
Paul Skok <paskok@davidson.edu>
2024-04-14 18:57:37 -0400

    more cleaning up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { FoodPage } from './pages/Food';
-import { MiscPage } from './pages/Misc';
-import { TranspoPage } from './pages/Transpo';
+
 import './App.css';
 
 function App() {
@@ -11,9 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Home /> } />
-        <Route path="/food" element={ <FoodPage /> } />
-        <Route path="/transpo" element={ <TranspoPage /> } />
-        <Route path="/misc" element={ <MiscPage /> } />
       </Routes>    
     </BrowserRouter>
   );
