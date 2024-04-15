05710263ef06153a2d15a1ed2c8cab52d7699bf5
awabid <awabid@davidson.edu>
2024-04-15 17:26:56 -0400

    new

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,21 +4,17 @@ import Navbar from '../components/Navbar'
 import { PostButton } from '../components/PostButton'
 import { ChatPage } from '../components/ChatPage'
 import { UserPoints } from '../components/UserPoints'
-import { ReplyBox } from '../components/ReplyBox'
 import '../styles/Home.css'
-
 export function Home() {
     const [chatMessages, setChatMessages] = useState<{ title: string; body: string; }[]>([]);
-
     const handlePost = (title: string, body: string) => {
         setChatMessages([...chatMessages, { title, body }]);
     };
-
     return (
         <div>
             <Navbar />
             <div className="container">
-                <div className="sidebar">
+                <div >
                     <Sidebar />
                 </div>
                 <div className="content">
