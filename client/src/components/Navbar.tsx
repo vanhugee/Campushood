9ee2cad0c3c7a16d2c04298f319ca0a94a349d0d
Paul Skok <paskok@davidson.edu>
2024-04-16 13:12:02 -0400

    cleaning up front end

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
 import campushoodLogo from "../assets/logo.png";
-import Sidebar from './Sidebar';
 import '../styles/Navbar.css'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-          <a href="/">
+        <a href="/home">
           <img src={campushoodLogo} alt="Campushood Logo" style={{ width: '100%', height: '100%' }}  />
-          </a>
+        </a>
       </div>
 
       <ul className="navbar-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/">Account</a></li>
+        <li><a href="/" className="account-link">Account</a></li>
       </ul>
+      <div className="popup-box">
+        <p>Hello, I am the popup.</p>
+      </div>
     </nav>
   );
 };
